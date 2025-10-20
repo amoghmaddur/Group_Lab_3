443e6917db5e3071837793ea448ff455631016d0
Prajwal-Prakash-FNU <lnu.prajw@northeastern.edu>
2025-10-19 23:01:33 -0400

    Department,Employer and Persona(M3 completed)

---
+++ Group_Lab_3/src/University/Persona/UserAccount.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package University.Persona;
+
+/**
+ *
+ * @author kal bugrara
+ */
+
+import University.Persona.Person;
+import info5100.university.example.workareas.Workarea;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class UserAccount {
+    Person person;
+    Workarea landingworkarea;
+    
+    public UserAccount(Person p){
+        person = p;
+    }
+    
+}
