6843319f9e067333699fe6c37a13994d7208e4e3
joshiishad <joshi.ishad@northeastern.edu>
2025-10-20 00:36:58 -0400

    employment history, faculty, workareas- m4 completed

---
+++ Group_Lab_3/src/University/Workareas/Workarea.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package University.Workareas;
+
+import University.Workareas.WorkRequest;
+import info5100.university.example.Persona.Person;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class Workarea {
+    
+    Person person; //owner
+    ArrayList<WorkRequest> inQueue;
+    ArrayList<WorkRequest> outQueue;
+    
+    public Workarea(Person p){
+        
+        person = p;
+        inQueue = new ArrayList();
+        outQueue = new ArrayList();
+        
+        
+    }
+    
+    public void addInQuestRequest(WorkRequest wr){
+        inQueue.add(wr);
+    }
+
+}
